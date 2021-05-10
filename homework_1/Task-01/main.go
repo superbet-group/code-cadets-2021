bb1fd745caad7b37a2467ff6700e6d36457519a5
jMilic <Jura.Milic@avl.com>
2021-05-10 22:59:39 +0200

    Fixed homework typo in module name

--- homework_1/Task-01/main.go
+++ homework_1/Task-01/main.go
@@ -1,1 +1,22 @@
-package Task_01
+package main
+
+import (
+	"log"
+
+	"github.com/pkg/errors"
+
+	"code-cadets-2021/homeword_1/Task-01/FizzBuzz"
+)
+
+func main() {
+	var start, end int
+
+	str, err := FizzBuzz.FizzBuzz(start, end)
+	if err != nil {
+		log.Fatal(
+			errors.WithMessage(err, "HTTP get towards yesno API"),
+		)
+	}
+
+	log.Printf("%s", str)
+}
