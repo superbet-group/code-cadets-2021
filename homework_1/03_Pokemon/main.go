d32e387896f1580f9d7c1108de8f47543b5f8c80
Matija Bertović <matbertovic@gmail.com>
2021-05-11 18:16:09 +0200

    added struct tags to work with json

--- homework_1/03_Pokemon/main.go
+++ homework_1/03_Pokemon/main.go
@@ -11,20 +11,20 @@ import (
 )
 
 type locationAreaEncounter struct {
-	Location_area struct {
+	LocationArea struct {
 		Name string
-	}
+	} `json:"location_area"`
 }
 
 type pokemon struct {
-	Name string
-	Locations []string
+	Name string `json:"name"`
+	Locations []string `json:"locations"`
 }
 
 const pokeapiURL = "https://pokeapi.co/api/v2"
 
 func main() {
-	name := flag.String("name", "", "Pokemon name.")
+	name := flag.String("name", "pikachu", "Pokemon name.")
 
 	flag.Parse()
 
@@ -54,10 +54,10 @@ func main() {
 	}
 
 	for _, location := range decodedContent {
-		poke.Locations = append(poke.Locations, location.Location_area.Name)
+		poke.Locations = append(poke.Locations, location.LocationArea.Name)
 	}
 
-	pokeJson, err := json.Marshal(poke)
+	pokeJson, err := json.MarshalIndent(poke, "", "    ")
 	if err != nil {
 		fmt.Println("Error marshalling the pokemon struct.")
 		os.Exit(1)
