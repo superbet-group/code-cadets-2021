bc85d88d1fcaee1a399720d495d69f504a0c348f
Matija Bertović <matbertovic@gmail.com>
2021-05-15 14:51:39 +0200

    tasks implementation

--- lecture_2/05_offerfeed/cmd/main.go
+++ lecture_2/05_offerfeed/cmd/main.go
@@ -1,51 +1,21 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"sync"
-	"time"
 
 	"code-cadets-2021/lecture_2/05_offerfeed/cmd/bootstrap"
+	"code-cadets-2021/lecture_2/05_offerfeed/internal/tasks"
 )
 
 func main() {
+	signalHandler := bootstrap.NewSignalHandler()
+
 	feed := bootstrap.NewAxilisOfferFeed()
 	queue := bootstrap.NewOrderedQueue()
 
 	feedProcessor := bootstrap.NewFeedProcessorService(feed, queue)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	wg := &sync.WaitGroup{}
-	wg.Add(3)
-
-	go func() {
-		defer wg.Done()
-		err := feed.Start(ctx)
-		if err != nil {
-			fmt.Println("feed error")
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		err := queue.Start(ctx)
-		if err != nil {
-			fmt.Println("queue error")
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		err := feedProcessor.Start(ctx)
-		if err != nil {
-			fmt.Println("processor error")
-		}
-	}()
-
-	wg.Wait()
+	tasks.RunTasks(signalHandler, feed, queue, feedProcessor)
 
 	fmt.Println("program finished gracefully")
 }
--- lecture_2/05_offerfeed/internal/tasks/run.go
+++ lecture_2/05_offerfeed/internal/tasks/run.go
@@ -2,12 +2,33 @@ package tasks
 
 import (
 	"context"
+	"fmt"
+	"sync"
 )
 
 func RunTasks(tasks ...Task) {
 	// run each task in separate goroutine
 	// wait for all tasks to finish
-	//
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(tasks))
+
+	for _, task := range tasks {
+		go func(task Task) {
+			defer wg.Done()
+			defer cancel()
+
+			err := task.Start(ctx)
+			if err != nil {
+				fmt.Println("task error")
+			}
+		}(task)
+	}
+
+	wg.Wait()
+
 	// when first task finishes, signal to the other goroutines that application should stop
 }
 
