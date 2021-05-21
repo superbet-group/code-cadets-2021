417fa61c5456b3c0b91b9f334b600f2fdff6f76c
Matija Bertović <matbertovic@gmail.com>
2021-05-21 09:19:37 +0200

    added http client timeout

--- homework_2/offerfeed/cmd/bootstrap/axilisOfferFeed.go
+++ homework_2/offerfeed/cmd/bootstrap/axilisOfferFeed.go
@@ -2,11 +2,12 @@ package bootstrap
 
 import (
 	stdhttp "net/http"
+	"time"
 
 	"code-cadets-2021/homework_2/offerfeed/internal/infrastructure/http"
 )
 
 func AxilisOfferFeed() *http.AxilisOfferFeed {
-	httpClient := &stdhttp.Client{}
+	httpClient := &stdhttp.Client{Timeout: time.Second * 10}
 	return http.NewAxilisOfferFeed(httpClient)
 }
--- homework_2/offerfeed/cmd/bootstrap/notAJsonFeed.go
+++ homework_2/offerfeed/cmd/bootstrap/notAJsonFeed.go
@@ -2,12 +2,13 @@ package bootstrap
 
 import (
 	stdhttp "net/http"
+	"time"
 
 	"code-cadets-2021/homework_2/offerfeed/internal/infrastructure/http"
 )
 
 func NotAJsonFeed() *http.NotAJsonFeed {
-	httpClient := &stdhttp.Client{}
+	httpClient := &stdhttp.Client{Timeout: time.Second * 10}
 	return http.NewNotAJsonFeed(httpClient)
 }
 
