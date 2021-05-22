135ae02d9598f290adbdd638b8388e99ac357f3d
Matija Bertović <matbertovic@gmail.com>
2021-05-22 13:10:31 +0200

    engine implementation

--- lecture_3/03_project/calculator/internal/engine/engine.go
+++ lecture_3/03_project/calculator/internal/engine/engine.go
@@ -1,9 +1,64 @@
 package engine
 
+import (
+	"context"
+	"log"
+)
+
 // Engine is the main component, responsible for consuming received bets and calculated bets,
 // processing them and publishing the resulting bets.
-/*type Engine struct {
+type Engine struct {
 	consumer  Consumer
 	handler   Handler
 	publisher Publisher
-}*/
+}
+
+// New creates and returns a new engine.
+func New(consumer Consumer, handler Handler, publisher Publisher) *Engine {
+	return &Engine{
+		consumer:  consumer,
+		handler:   handler,
+		publisher: publisher,
+	}
+}
+
+// Start will run the engine.
+func (e *Engine) Start(ctx context.Context) {
+	err := e.processBets(ctx)
+	if err != nil {
+		log.Println("Engine failed to process bets:", err)
+		return
+	}
+
+	err = e.processEventUpdates(ctx)
+	if err != nil {
+		log.Println("Engine failed to process event updates:", err)
+		return
+	}
+
+	<-ctx.Done()
+}
+
+func (e *Engine) processBets(ctx context.Context) error {
+	consumedBetsReceived, err := e.consumer.ConsumeBets(ctx)
+	if err != nil {
+		return err
+	}
+
+	resultingBets := e.handler.HandleBets(ctx, consumedBetsReceived)
+	e.publisher.PublishBetsCalculated(ctx, resultingBets)
+
+	return nil
+}
+
+func (e *Engine) processEventUpdates(ctx context.Context) error {
+	consumedBetsCalculated, err := e.consumer.ConsumeEventUpdates(ctx)
+	if err != nil {
+		return err
+	}
+
+	resultingBets := e.handler.HandleEventUpdates(ctx, consumedBetsCalculated)
+	e.publisher.PublishBetsCalculated(ctx, resultingBets)
+
+	return nil
+}
