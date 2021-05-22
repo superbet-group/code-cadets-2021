49f3717ebb678d31c32f3360a4aa237e68932f14
Matija Bertović <matbertovic@gmail.com>
2021-05-22 11:22:20 +0200

    db init

---
+++ lecture_3/03_project/calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/superbet-group/code-cadets-2021/lecture_3/03_project/calculator
+
+go 1.14
---
+++ lecture_3/03_project/calculator/internal/engine/engine.go
@@ -0,0 +1,9 @@
+package engine
+
+// Engine is the main component, responsible for consuming received bets and calculated bets,
+// processing them and publishing the resulting bets.
+/*type Engine struct {
+	consumer  Consumer
+	handler   Handler
+	publisher Publisher
+}*/
